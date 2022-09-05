79c9e8c918f9592095ee6d6ac8274765d0bdcdd2
PaulinaMales <87955476+PaulinaMales@users.noreply.github.com>
2022-09-05 00:12:47 -0500

    Eliminando archivos no deseados

--- src/Administrador.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.*;
-
-public class Administrador {
-    public JPanel administratorPanel;
-
-
-}
--- src/Bodeguero.java
+++
@@ -1,5 +0,0 @@
-import javax.swing.*;
-
-public class Bodeguero {
-    public JPanel winemakerPanel;
-}
